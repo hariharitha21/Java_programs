f240393f1f9e8b360016baa81527cf502284a78f
Haritha Harigovindan <42141035+hariharitha21@users.noreply.github.com>
2021-09-08 19:22:57 +0530

    Create pattern3.java
             1
            121
           12321
          1234321
         123454321
        12345654321
       1234567654321
      123456787654321
     12345678987654321
    12345678910987654321

---
+++ pattern3.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Numbers{
+    public static void main(String[] args){
+        int i,j,l,k,num;
+        Scanner input=new Scanner(System.in);
+        num=input.nextInt();
+        for(i=1;i<=num;i++){
+            for (k=num-i;k>0;k--){
+                System.out.print(" ");
+            }
+            for(j=1;j<=i;j++){ 
+                System.out.print(j);
+            }
+            for(l=j-2;l>0;l--){ 
+                System.out.print(l);
+            }
+            System.out.println();
+        }
+    }
+}
