ee5aa7df345e4a0779003bac17ca6419096b5766
Haritha Harigovindan <42141035+hariharitha21@users.noreply.github.com>
2021-09-08 18:14:05 +0530

    Create n_intigers.java

---
+++ n_intigers.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Numbers{
+    public static void main(String[] args){
+        int i,num;
+        Scanner input=new Scanner(System.in);
+        num=input.nextInt();
+        for(i=0;i<num;i++){
+            System.out.println(i);
+        }
+    }
+}
